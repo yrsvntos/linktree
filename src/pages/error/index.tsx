44c882eb05c89f01c5c8de6b69bff149114e73a3
Yanik dos Santos <yrsvntos@gmail.com>
2025-08-25 19:43:01 +0100

    feat(páginas): criação das páginas [error, networks]

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+
 export function Error(){
     return(
-        <>
-            <h1>Teste página Erro</h1>
-        </>
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <h1 className="font-bold text-4xl text-white">ERRO</h1>
+            <h1 className="font-bold text-8xl text-white my-8">404</h1>
+            <p className="text-white font-medium text-xl">Oooops!! A página que você está a procura não existe!</p>
+            <Link to="/" className="mt-8 bg-zinc-600 text-white p-2 px-4 rounded-md cursor-pointer">
+                Voltar ao Início
+            </Link>
+        </div>
     );
 }
--- src/pages/networks/index.tsx
+++ src/pages/networks/index.tsx
@@ -1,7 +1,51 @@
+import { useState, type FormEvent } from "react";
+import { Header } from "../../components/header";
+import { Input } from "../../components/input";
+import { FiLink } from "react-icons/fi";
+
 export function RedesSociais(){
+
+    const [linkedin, setLinkedin] = useState("");
+    const [instagram, setInstagram] = useState("");
+    const [github, setGithub] = useState("");
+
+    function handleRegister(e: FormEvent){
+        e.preventDefault();
+        alert('Teste');
+        return;
+    }
+
     return(
-        <>
-            <h1>Teste página Redes Sociais</h1>
-        </>
+        <div className="flex flex-col items-center  justify-center">
+            <Header/>
+            <h1 className="text-white text-3xl mt-8">Suas redes sociais</h1>
+
+            <form onSubmit={handleRegister} className="flex flex-col w-full max-w-xl mt-7">
+                <label className="text-white font-medium mb-2">Link do Linkedin</label>
+                <Input
+                    type="url"
+                    placeholder="Digite a url"
+                    value={linkedin}
+                    onChange={(e) => setLinkedin(e.target.value)}
+                />
+                <label className="text-white font-medium mb-2">Link do Instagram</label>
+                <Input
+                    type="url"
+                    placeholder="Digite a url"
+                    value={instagram}
+                    onChange={(e) => setInstagram(e.target.value)}
+                />
+                <label className="text-white font-medium mb-2">Link do Github</label>
+                <Input
+                    type="url"
+                    placeholder="Digite a url"
+                    value={github}
+                    onChange={(e) => setGithub(e.target.value)}
+                />
+                <button className="rounded-md p-2 bg-blue-600 text-white flex items-center gap-2 justify-center">
+                    Salvar links <FiLink/>
+                </button>
+            </form>
+        </div>
     );
 }
