642cddb2d3930064facebe3bdc2a761d3f096057
Yanik dos Santos <yrsvntos@gmail.com>
2025-08-27 05:47:16 +0100

    feat: listagem dos links na página principal

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,52 @@
+import { useState, useEffect } from "react";
 import { FiInstagram, FiGithub, FiLinkedin } from "react-icons/fi";
+import { db } from "../services/firebaseConnection";
+import { 
+    getDocs,
+    collection,
+    orderBy,
+    query,
+    doc,
+    getDoc,
+    onSnapshot
+
+} from "firebase/firestore";
+
+interface LinkProps{
+    id: string;
+    name: string;
+    url: string;
+    color: string;
+    bg: string;
+}
 
 export function Home(){
+
+    const [links, setLink] = useState<LinkProps []>([]);
+
+    useEffect(()=>{
+       function loadLinks(){
+            const getRef = collection(db, "links");
+            const queryRef = query(getRef, orderBy("created", "asc"));
+
+            getDocs(queryRef)
+            .then((snapshot)=>{
+                let lista = [] as LinkProps[];
+                snapshot.forEach((doc)=>{
+                    lista.push({
+                        id: doc.id,
+                        name: doc.data().name,
+                        url: doc.data().url,
+                        color: doc.data().color,
+                        bg: doc.data().bg
+                    })
+                })
+                setLink(lista)
+            })
+       }
+
+       loadLinks();
+    }, [])
     return(
         <div 
             className="flex flex-col w-full py-4 items-center justify-center h-screen"
@@ -8,11 +54,18 @@ export function Home(){
             <h1 className="md:text-4xl text-3xl font-bold text-white mt-20">Yanik dos Santos</h1>
             <p className="text-gray-50 mb-5 mt-3">Veja os meus links</p>
             <main className="flex flex-col w-11/12 max-w-xl text-center">
-                <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
-                    <a href="">
-                        <p>Meu Linkedin</p>
-                    </a>
-                </section>
+                {links.map((link) => (
+                    <section 
+                        className="mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer"
+                        key={link.id}
+                        style={{backgroundColor: link.bg, color: link.color}}
+                    >
+                        <a href={link.url}>
+                            <p>{link.name}</p>
+                        </a>
+                    </section>
+                ))}
+                
                 <footer className="flex justify-center my-3 gap-4">
                     <a href="">
                         <FiInstagram size={28} color="#fff"/>
